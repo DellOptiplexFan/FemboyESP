c5e9dc0c41c07d63587b9c08ed4f5f43663bbcc7
DellOptiplexFan <134756793+DellOptiplexFan@users.noreply.github.com>
2024-10-28 20:32:24 -0700

    v2 has released! now with 4 new femboys!

--- !!femboyesp.lua
+++ !!femboyesp.lua
@@ -1,12 +1,30 @@
 -- femboy esp
 
 local femboy = render.load_image('primordial\\scripts\\femboy.jpg')
+local femboy2 = render.load_image('primordial\\scripts\\femboy2.jpg')
+local femboy3 = render.load_image('primordial\\scripts\\femboy3.jpg')
+local femboy4 = render.load_image('primordial\\scripts\\femboy4.jpg')
+local femboy5 = render.load_image('primordial\\scripts\\femboy5.png')
 
-local opacity = menu.add_slider("opacity", "opacity", 0, 255)
+local opacity = menu.add_slider("visuals", "opacity", 0, 255)
+local selection = menu.add_selection("visuals", "femboy image", {"femboy1", "femboy2", "femboy3", "femboy4", "femboy5"})
+selection:set(1)
 opacity:set(255)
 
--- Debug to check what type `femboy` actually is
-print("Type of femboy:", type(femboy))
+local function get_selected_femboy()
+    local selected = selection:get()
+    if selected == 1 then
+        return femboy
+    elseif selected == 2 then
+        return femboy2
+    elseif selected == 3 then
+        return femboy3
+    elseif selected == 4 then
+        return femboy4
+    else
+        return femboy5
+    end
+end
 
 local function render_image_on_enemy()
     local local_player = entity_list.get_local_player()  -- Get the local player
@@ -33,7 +51,8 @@ local function render_image_on_enemy()
                 local centered_pos = vec2_t(screen_pos.x - width / 2, screen_pos.y - height / 2)
 
                 -- Render the texture at the centered position with scaled size
-                render.texture(femboy.id, centered_pos, vec2_t(width, height), color_t(255, 255, 255, opacity:get()))
+                local selected_femboy = get_selected_femboy()
+                render.texture(selected_femboy.id, centered_pos, vec2_t(width, height), color_t(255, 255, 255, opacity:get()))
             end
         end
     end
